c67089bb01e41dd7b23b58b33cdc720d5f504658
Golovolastik <yankovich.aliaksei@gmail.com>
2026-02-03 16:27:22 +0300

    Init project

---
+++ Url-shortener/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Url_shortener.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
